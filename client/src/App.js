95066df95542fa7c21bcb7fc85b2a0d98aaf4023
Andrew Whipple <android128@gmail.com>
2020-11-27 13:16:27 -0600

    Add support for adding new episodes, and fixed bug on single-episode feed

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,7 @@ class EditableLine extends React.Component {
   }
 
   handleEditButtonClick(event) {
-    console.log("Clicked!");
+    console.log("Handle edit button Clicked!");
     this.setState({editable: true});
   }
 
@@ -143,12 +143,18 @@ class ChannelAttributes extends React.Component {
     var attributeLines = []
 
     for (var attrKey in this.state.channelJSON) {
-      console.log(attrKey);
+      console.log("Channel attrkey: " + attrKey);
 
       if (attrKey !== "item") {
         var label = attrKey + ": ";
 
         attributeLines.push(<EditableLine key={attrKey + "_component_key"} label={label} currentEntry={this.state.channelJSON[attrKey]._text} updateFeedJSON={this.updateChannelJSON} fieldKey={attrKey} />)
+      } else {
+        if (!Array.isArray(this.state.channelJSON[attrKey])) {
+          var newChannelJSON = this.state.channelJSON;
+          newChannelJSON.item = [this.state.channelJSON.item];
+          this.setState({channelJSON: newChannelJSON});
+        }
       }
     }
 
@@ -174,18 +180,61 @@ class ItemList extends React.Component {
       itemArray: this.props.itemArray
     }
 
-    this.updateItemArray = this.updateItemArray.bind(this);
+    this.addNewItem = this.addNewItem.bind(this);
 
   }
 
-  updateItemArray() {
-    console.log('updateItemArray() not written yet!');
+  
+
+  addNewItem() {
+    console.log("Add new item clicked for : " + this.state.itemArray[0]);
+    var newBlankItem = {
+      'description': {
+        '_cdata': ''
+      },
+      'enclosure': {
+        '_attributes': {
+          'length': '',
+          'type': '',
+          'url': ''
+        }
+      },
+      'guid': {
+        '_attributes': {
+          'isPermalink': ''
+        },
+        '_text': ''
+      },
+      'itunes:author': {
+        '_text': ''
+      },
+      'itunes:duration': {
+        '_text': ''
+      },
+      'itunes:subtitle': {
+        '_text': ''
+      },
+      'itunes:summary': {
+        '_cdata': ''
+      },
+      'pubDate': {
+        '_text': ''
+      },
+      'title': {
+        '_text': ''
+      }
+    };
+
+    var newItemArray = this.state.itemArray;
+    newItemArray.push(newBlankItem);
+    this.setState({itemArray: newItemArray});
   }
 
   render() {
     var itemAttributesArray = [];
     for (var item in this.state.itemArray) {
-      console.log(this.state.itemArray[item]);
+      console.log("Item array: " + this.state.itemArray);
+      //console.log("pushing to the item render array: " + this.state.itemArray[item]);
       itemAttributesArray.push(<ItemAttributes itemJSON={this.state.itemArray[item]}/>);
     }
 
@@ -193,6 +242,8 @@ class ItemList extends React.Component {
     return(
       <div>
         {itemAttributesArray}
+        <hr/>
+        <button type="button" onClick={this.addNewItem}>Add new episode</button>
       </div>
     )
   }
@@ -236,25 +287,26 @@ class ItemAttributes extends React.Component {
     var attributeLines = [];
 
     for (var attrKey in this.state.itemJSON) {
-      console.log(attrKey);
+      console.log("Item attribute key: " + attrKey);
 
       if (attrKey === 'enclosure') {
         var label = attrKey + " - ";
 
-        attributeLines.push(<EditableLine key={attrKey + "url_item_key_" + this.state.itemJSON.guid._text} label={label + 'url: '} currentEntry={this.state.itemJSON[attrKey]._attributes.url} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.url'} />)
-        attributeLines.push(<EditableLine key={attrKey + "length_item_key_" + this.state.itemJSON.guid._text} label={label + 'length: '} currentEntry={this.state.itemJSON[attrKey]._attributes.length} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.length'} />)
-        attributeLines.push(<EditableLine key={attrKey + "type_item_key_" + this.state.itemJSON.guid._text} label={label + 'type: '} currentEntry={this.state.itemJSON[attrKey]._attributes.type} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.type'} />)
+        attributeLines.push(<EditableLine key={attrKey + "url_item_key_" + this.state.itemJSON.guid._text} label={label + 'url: '} currentEntry={this.state.itemJSON[attrKey]._attributes.url} updateFeedJSON={this.updateItemJSON || " "} fieldKey={attrKey + '.url'} />)
+        attributeLines.push(<EditableLine key={attrKey + "length_item_key_" + this.state.itemJSON.guid._text} label={label + 'length: '} currentEntry={this.state.itemJSON[attrKey]._attributes.length || " "} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.length'} />)
+        attributeLines.push(<EditableLine key={attrKey + "type_item_key_" + this.state.itemJSON.guid._text} label={label + 'type: '} currentEntry={this.state.itemJSON[attrKey]._attributes.type || " "} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.type'} />)
 
       } else if (attrKey === 'guid') {
 
         var label = attrKey + " - ";
 
-        attributeLines.push(<EditableLine key={attrKey + "isPermalink_item_key_" + this.state.itemJSON.guid._text} label={label + 'isPermalink: '} currentEntry={this.state.itemJSON[attrKey]._attributes.isPermalink} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.isPermalink'} />)
-        attributeLines.push(<EditableLine key={attrKey + "text_item_key_" + this.state.itemJSON.guid._text} label={attrKey + ': '} currentEntry={this.state.itemJSON[attrKey]._text} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey} />)
+        attributeLines.push(<EditableLine key={attrKey + "isPermalink_item_key_" + this.state.itemJSON.guid._text} label={label + 'isPermalink: '} currentEntry={this.state.itemJSON[attrKey]._attributes.isPermalink || " "} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey + '.isPermalink'} />)
+        attributeLines.push(<EditableLine key={attrKey + "text_item_key_" + this.state.itemJSON.guid._text} label={attrKey + ': '} currentEntry={this.state.itemJSON[attrKey]._text || " "} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey} />)
 
       } else {
         var label = attrKey + ": ";
-        attributeLines.push(<EditableLine key={attrKey + "_item_key_" + this.state.itemJSON.guid._text} label={label} currentEntry={this.state.itemJSON[attrKey]._text || this.state.itemJSON[attrKey]._cdata} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey} />)
+        console.log("current state: " + this.state.itemJSON);
+        attributeLines.push(<EditableLine key={attrKey + "_item_key_" + this.state.itemJSON.guid._text} label={label} currentEntry={this.state.itemJSON[attrKey]._text || this.state.itemJSON[attrKey]._cdata || " "} updateFeedJSON={this.updateItemJSON} fieldKey={attrKey} />)
       }
       
     }
@@ -340,18 +392,13 @@ class FeedBuilder extends React.Component {
         "Content-type": "application/json"
       }
     }).then((res) => {
-      console.log(res);
       res.text().then((xmlResponse) => {
         if(res.ok) {
-          console.log(xmlResponse);
 
           var blob = new Blob([xmlResponse], { type: 'text/xml'});
-          
           var url = window.URL.createObjectURL(blob);
-        
+  
           var link = document.createElement('a');
-
-          console.log(link);
           link.download = 'feed.xml';
           link.href = url;
 
